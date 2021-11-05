4bb49c421055a80edf4f9b80453d9c59f7278acc
Avery Biskup <averybiskup@gmail.com>
2021-11-04 20:00:06 -0700

    added assignment2 files

---
+++ project.js
@@ -0,0 +1,67 @@
+import {defs, tiny} from './examples/common.js';
+
+const {
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene,
+} = tiny;
+
+class Base_Scene extends Scene {
+    /**
+     *  **Base_scene** is a Scene that can be added to any display canvas.
+     *  Setup the shapes, materials, camera, and lighting here.
+     */
+    constructor() {
+        // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
+        super();
+        // At the beginning of our program, load one of each of these shape definitions onto the GPU.
+
+        // *** Materials
+        this.materials = {
+            plastic: new Material(new defs.Phong_Shader(),
+                {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
+        };
+        // The white material and basic shader are used for drawing the outline.
+        this.white = new Material(new defs.Basic_Shader());
+    }
+
+    display(context, program_state) {
+        // display():  Called once per frame of animation. Here, the base class's display only does
+        // some initial setup.
+
+        // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
+        if (!context.scratchpad.controls) {
+            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // Define the global camera and projection matrices, which are stored in program_state.
+            program_state.set_camera(Mat4.translation(5, -10, -30));
+        }
+        program_state.projection_transform = Mat4.perspective(
+            Math.PI / 4, context.width / context.height, 1, 100);
+
+        // *** Lights: *** Values of vector or point lights.
+        const light_position = vec4(0, 5, 5, 1);
+        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+    }
+}
+export class Project extends Base_Scene {
+    /**
+     * This Scene object can be added to any display canvas.
+     * We isolate that code so it can be experimented with on its own.
+     * This gives you a very small code sandbox for editing a simple scene, and for
+     * experimenting with matrix transformations.
+     */
+
+    constructor() {
+        super();
+    }
+
+    make_control_panel() {
+
+        // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
+        this.key_triggered_button("Test", ["c"], () => console.log('test'));
+    }
+
+    display(context, program_state) {
+        super.display(context, program_state);
+        let model_transform = Mat4.identity();
+
+    }
+}
