2c16462a2da76bf541d5702d7f875d4ccb473b29
Yuxi Chang <michaelchangyx@g.ucla.edu>
2021-11-09 14:50:08 -0800

    Add maze ground and boarder to base scene

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cs174A-project
-Test
+## Maze Size
+The maze is on x-z plane from (0, 0) to (120, 80) 
+each grid will be 2x2 size so there are 60 x 40 grids. 
--- project.js
+++ project.js
@@ -13,16 +13,44 @@ class Base_Scene extends Scene {
         // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
         super();
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
-
+        this.shapes = {
+            maze_ground: new defs.Square(),
+            boarder: new defs.Cube(),
+        }
         // *** Materials
         this.materials = {
-            plastic: new Material(new defs.Phong_Shader(),
-                {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
+            ground: new Material(new defs.Phong_Shader(),
+                {ambient: .9, diffusivity: .6, color: hex_color("#ffffed")}),
+            white_plastic: new Material(new defs.Phong_Shader(), 
+                {ambient: 1.0, diffusivity:.2, color: hex_color('#ffffff')}), //place holder for now
         };
         // The white material and basic shader are used for drawing the outline.
         this.white = new Material(new defs.Basic_Shader());
     }
+    draw_maze_ground(context, program_state){ //on x-z plane
+        let model_transform = Mat4.identity();
+        model_transform = model_transform.times(Mat4.scale(2000.0, 1.0, 2000.0))
+                                         //.times(Mat4.translation(1.0, 0.0, 1.0))
+                                         .times(Mat4.rotation(Math.PI/2, 1, 0, 0));
+        this.shapes.maze_ground.draw(context, program_state, model_transform, this.materials.ground);
+    }
 
+    draw_maze_boarder(context, program_state, width, height){ //available area: z: 0 -- height; x: 0 -- width
+        let model_transform = Mat4.identity();
+        model_transform = model_transform.times(Mat4.scale(1.0, 1.0, height/2))
+                                         .times(Mat4.translation(-1.0, 1.0, 1.0))
+        this.shapes.boarder.draw(context, program_state, model_transform, this.materials.white_plastic);
+        model_transform = model_transform.times(Mat4.translation(width+2, 0, 0));
+        this.shapes.boarder.draw(context, program_state, model_transform, this.materials.white_plastic);
+        model_transform = Mat4.identity();
+        model_transform = model_transform.times(Mat4.translation(-2.0, 0, 0))
+                                         .times(Mat4.scale(width/2+2, 1.0, 1.0))
+                                         .times(Mat4.translation(1.0, 1.0, -1.0));
+        this.shapes.boarder.draw(context, program_state, model_transform, this.materials.white_plastic);
+        model_transform = model_transform.times(Mat4.translation(0, 0, height+2));
+        this.shapes.boarder.draw(context, program_state, model_transform, this.materials.white_plastic);
+    }
+    
     display(context, program_state) {
         // display():  Called once per frame of animation. Here, the base class's display only does
         // some initial setup.
@@ -31,14 +59,16 @@ class Base_Scene extends Scene {
         if (!context.scratchpad.controls) {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
             // Define the global camera and projection matrices, which are stored in program_state.
-            program_state.set_camera(Mat4.translation(5, -10, -30));
+            program_state.set_camera(Mat4.look_at(vec3(60, 110, 20), vec3(60, 0, 40), vec3(0, 1, 0)));
         }
         program_state.projection_transform = Mat4.perspective(
-            Math.PI / 4, context.width / context.height, 1, 100);
+            Math.PI / 4, context.width / context.height, 1, 200);
 
         // *** Lights: *** Values of vector or point lights.
         const light_position = vec4(0, 5, 5, 1);
         program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+        this.draw_maze_ground(context, program_state);
+        this.draw_maze_boarder(context, program_state, 120, 80); //draw a 120 x 80 area on x-z plane
     }
 }
 export class Project extends Base_Scene {
